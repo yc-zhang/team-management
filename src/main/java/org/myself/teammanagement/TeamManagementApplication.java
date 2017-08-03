6ca614d41ae5d613b829803d8b2445b528e6804b
yc-zhang <turalyon.zhangyc@gmail.com>
2017-08-03 11:13:13 +0800

    The skeleton of this simple app.

---
+++ src/main/java/org/myself/teammanagement/TeamManagementApplication.java
@@ -0,0 +1,12 @@
+package org.myself.teammanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamManagementApplication.class, args);
+	}
+}
